c1c63526be99aece2df4f1595a1c25ec14e62bf1
kimjanghu <kjh921030@gmail.com>
2021-08-01 16:18:29 +0900

    feat: 팀원별 TodoList 불러오기

--- src/js/components/Todo/TodoList/TodoItem.js
+++ src/js/components/Todo/TodoList/TodoItem.js
@@ -1,15 +1,40 @@
+import { PRIORITY_TYPE } from "../../../utils/constants.js";
+
 export default function TodoItem({ _id, contents, isCompleted, priority }) {
+  const setChipView = () => {
+    return `
+      <select class="chip select ${setSelectboxColor(priority)}">
+        <option value="0" ${checkSelected(priority, PRIORITY_TYPE.NONE)}>순위</option>
+        <option value="1" ${checkSelected(priority, PRIORITY_TYPE.FIRST)}>1순위</option>
+        <option value="2" ${checkSelected(priority, PRIORITY_TYPE.SECOND)}>2순위</option>
+      </select>
+    `;
+  };
+
+  const setSelectboxColor = () => {
+    switch (priority) {
+      case PRIORITY_TYPE.FIRST:
+        return "primary";
+      case PRIORITY_TYPE.SECOND:
+        return "secondary";
+      default:
+        return "";
+    }
+  };
+
+  const checkSelected = (value) => (priority === value ? "selected" : "");
+
+  const checkCompleteChecked = () => (isCompleted ? "checked" : "");
+
+  const checkCompleteClass = () => (isCompleted ? "completed" : "");
+
   return `
-    <li data-todo-id=${_id} class="todo-list-item">
+    <li data-todo-id=${_id} class="todo-list-item ${checkCompleteClass()}">
       <div class="view">
-        <input class="toggle" type="checkbox" />
+        <input class="toggle" type="checkbox" ${checkCompleteChecked()}/>
         <label class="label">
           <div class="chip-container">
-            <select class="chip select">
-              <option value="0" selected>순위</option>
-              <option value="1">1순위</option>
-              <option value="2">2순위</option>
-            </select>
+            ${setChipView()}
           </div>
           ${contents}
         </label>
